f0cff6df25d5700d218f7b238e0bb03e520f84dd
wangcong1994 <779033969@qq.com>
2021-03-26 16:57:58 +0800

    按需加载样式

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<template>
+  <van-image :src="image"></van-image>
+</template>
+
+<script setup>
+import image from "../assets/logo.png";
+</script>
+<style scoped></style>
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import styleImport from "vite-plugin-style-import";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // 按需加载样式
+    styleImport({
+      libs: [
+        {
+          libraryName: "vant",
+          esModule: true,
+          resolveStyle: (name) => {
+            return `vant/es/${name}/index.css`;
+          },
+        },
+      ],
+    }),
+  ],
+});
